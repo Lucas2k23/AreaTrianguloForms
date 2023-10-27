080c571210c46b7938d2b0d1fa853f754fc57972
Lucas Araujo <149210048+Lucas2k23@users.noreply.github.com>
2023-10-27 20:57:41 -0300

    Interface e Código concluídos
    Foi finalizada a interface e implementado o código com tratamento de erro.

--- AreaTrianguloForms/Form1.Designer.cs
+++ AreaTrianguloForms/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.txbNum1 = new System.Windows.Forms.TextBox();
+            this.txbNum2 = new System.Windows.Forms.TextBox();
+            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(38, 136);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(173, 33);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Calcular";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Location = new System.Drawing.Point(35, 44);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(34, 13);
+            this.lblNum1.TabIndex = 1;
+            this.lblNum1.Text = "Base:";
+            // 
+            // txbNum1
+            // 
+            this.txbNum1.Location = new System.Drawing.Point(75, 41);
+            this.txbNum1.Name = "txbNum1";
+            this.txbNum1.Size = new System.Drawing.Size(100, 20);
+            this.txbNum1.TabIndex = 2;
+            // 
+            // txbNum2
+            // 
+            this.txbNum2.Location = new System.Drawing.Point(75, 85);
+            this.txbNum2.Name = "txbNum2";
+            this.txbNum2.Size = new System.Drawing.Size(100, 20);
+            this.txbNum2.TabIndex = 3;
+            // 
+            // txbResultado
+            // 
+            this.txbResultado.Enabled = false;
+            this.txbResultado.Location = new System.Drawing.Point(38, 191);
+            this.txbResultado.Name = "txbResultado";
+            this.txbResultado.Size = new System.Drawing.Size(173, 20);
+            this.txbResultado.TabIndex = 4;
+            this.txbResultado.TextChanged += new System.EventHandler(this.txbResultado_TextChanged);
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Location = new System.Drawing.Point(32, 85);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(37, 13);
+            this.lblNum2.TabIndex = 5;
+            this.lblNum2.Text = "Altura:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(0, 194);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(32, 13);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "Area:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(254, 271);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.txbResultado);
+            this.Controls.Add(this.txbNum2);
+            this.Controls.Add(this.txbNum1);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.btn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Calcular Area";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.TextBox txbNum1;
+        private System.Windows.Forms.TextBox txbNum2;
+        private System.Windows.Forms.TextBox txbResultado;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
 
--- AreaTrianguloForms/Form1.cs
+++ AreaTrianguloForms/Form1.cs
@@ -16,5 +16,43 @@ namespace AreaTrianguloForms
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double n1, n2, area;
+
+                //Obter os valores dos textboxes:
+                n1 = double.Parse(txbNum1.Text);
+                n2 = double.Parse(txbNum2.Text);
+
+                area = (n1 * n2 / 2);
+
+                //Mostrar o resultado no txbResultado
+                txbResultado.Text = area.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Dados invalidos informados");
+                
+                // Limpar os txbs:
+                txbResultado.Clear();
+                txbNum1.Clear();
+                txbNum2.Clear();
+            }
+            
+
+        }
     }
 }
